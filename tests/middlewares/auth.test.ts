a947a59ce9bea1f853b5c6e46d1de48f31083805
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-10 01:45:05 +0900

    Feat: auth middleware 테스트 코드 작성

---
+++ tests/middlewares/auth.test.ts
@@ -0,0 +1,49 @@
+import express, { Express, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import request from "supertest";
+
+import config from "@/config";
+import { AuthPayload } from "@/interfaces/jwt.interface";
+import authMiddleware from "@/middlewares/auth.middleware";
+
+describe("JWT 인증 미들웨어", () => {
+  let app: Express;
+
+  beforeAll(() => {
+    app = express();
+    app.use(authMiddleware);
+    app.get("/", (req: Request, res: Response) => {
+      res.send(req.auth);
+    });
+  });
+
+  it("토큰이 존재하지 않으면 401을 반환한다", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe("토큰을 찾을 수 없습니다.");
+  });
+
+  it("토큰이 Bearer로 시작하지 않으면 401을 반환한다", async () => {
+    const res = await request(app).get("/").set("Authorization", "blablabla");
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe("토큰을 찾을 수 없습니다.");
+  });
+
+  it("토큰이 유효하지 않으면 401을 반환한다", async () => {
+    const res = await request(app).get("/").set("Authorization", "Bearer invalid.token");
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe("토큰이 유효하지 않습니다.");
+  });
+
+  it("토큰이 유효하면 다음 미들웨어를 실행한다", async () => {
+    const payload: AuthPayload = {
+      id: "test",
+      name: "Tester",
+      role: "staff",
+    };
+    const token = jwt.sign(payload, config.jwt.secretKey);
+    const res = await request(app).get("/").set("Authorization", `Bearer ${token}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expect.objectContaining(payload));
+  });
+});
