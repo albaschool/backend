dcba0f8107ba52e76be86cce1c9756e005480020
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-10 01:50:57 +0900

    Fix: 테스트 케이스 표현 통일

--- tests/middlewares/auth.test.ts
+++ tests/middlewares/auth.test.ts
@@ -17,25 +17,25 @@ describe("JWT 인증 미들웨어", () => {
     });
   });
 
-  it("토큰이 존재하지 않으면 401을 반환한다", async () => {
+  it("토큰이 존재하지 않으면 401 반환", async () => {
     const res = await request(app).get("/");
     expect(res.status).toBe(401);
     expect(res.body.message).toBe("토큰을 찾을 수 없습니다.");
   });
 
-  it("토큰이 Bearer로 시작하지 않으면 401을 반환한다", async () => {
+  it("토큰이 Bearer로 시작하지 않으면 401 반환", async () => {
     const res = await request(app).get("/").set("Authorization", "blablabla");
     expect(res.status).toBe(401);
     expect(res.body.message).toBe("토큰을 찾을 수 없습니다.");
   });
 
-  it("토큰이 유효하지 않으면 401을 반환한다", async () => {
+  it("토큰이 유효하지 않으면 401 반환", async () => {
     const res = await request(app).get("/").set("Authorization", "Bearer invalid.token");
     expect(res.status).toBe(401);
     expect(res.body.message).toBe("토큰이 유효하지 않습니다.");
   });
 
-  it("토큰이 유효하면 다음 미들웨어를 실행한다", async () => {
+  it("토큰이 유효하면 다음 미들웨어 실행", async () => {
     const payload: AuthPayload = {
       id: "test",
       name: "Tester",
