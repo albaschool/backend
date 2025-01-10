2d2f0c49d309aa7277f34fd5168a8ffd2d872776
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-10 14:09:17 +0900

    Feat: swagger 수정

--- src/routes/stores.route.ts
+++ src/routes/stores.route.ts
@@ -7,16 +7,52 @@ const router = express.Router();
 
 router.get(
   "/",
-  // #swagger.tags = ['Stores']
-  // #swagger.description = '모든 가게를 조회합니다.'
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "모든 가게를 조회합니다."
+  /*
+    #swagger.responses[404] = {
+      description: "Not Found",
+      content: {
+        "application/json": {
+          example: []
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
   getStores,
 );
 
 router.get(
   "/me",
-  // #swagger.tags = ['Stores']
-  // #swagger.description = '내 가게를 조회합니다.'
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "내 가게를 조회합니다."
   // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.responses[404] = {
+      description: "Not Found",
+      content: {
+        "application/json": {
+          example: []
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
   authMiddleware,
   getMyStores,
 );
