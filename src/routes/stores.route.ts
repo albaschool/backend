2ffe783fac464788ef2c44fb95998fd2096009ee
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-10 20:17:18 +0900

    Feat: 가게 엔드포인트 구현 (#6)
    * Fix: logger url 잘못 표시하는 현상 해결
    
    * Fix: Swagger endpointFile 수정
    
    * Feat: Stores swagger tag 생성
    
    * Feat: /stores 엔드포인트 구현
    
    * Feat: swagger security bearer 세팅
    
    * Feat: /stores/me 엔드포인트 구현
    
    * Feat: /stores 엔드포인트 404 리턴 형식 변경
    
    * Feat: swagger 수정
    
    * Fix: 서비스 네이밍 수정
    
    * Fix: /stores 엔드포인트 authMiddleware 추가
    
    * Feat: /stores/:id /stores/:id/members 엔드포인트 구현
    
    * Feat: POST /stores 엔드포인트 구현
    
    * Feat: DELETE /stores/:storeId/members/:memberId 엔드포인트 구현
    
    * Fix: return 메시지 변경
    
    * Feat: POST /stores/:storeId/members 엔드포인트 구현
    
    * Fix: id -> storeId parameter 수정
    
    * Fix: 디버그용 쿼리 로깅 제거
    
    * Feat: jsdoc
    
    * Style: prettier
    
    * Feat: example ref 사용
    
    * Style: 코드 순서 swagger 정렬과 일치
    
    * Feat: 존재하는 사용자인지 확인

---
+++ src/routes/stores.route.ts
@@ -0,0 +1,335 @@
+import express from "express";
+
+import {
+  addStoreMember,
+  createStore,
+  deleteStoreMember,
+  getMyStores,
+  getStoreById,
+  getStoreMembers,
+  getStores,
+} from "@/controllers/stores.controller";
+import authMiddleware from "@/middlewares/auth.middleware";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "모든 가게를 조회합니다."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.responses[200] = {
+      description: "OK",
+      content: {
+        "application/json": {
+          examples: {
+            example: { $ref: "#/components/examples/storesExample" }
+          }
+        }
+      }
+    }
+    #swagger.responses[404] = {
+      description: "가게가 존재하지 않을 때",
+      content: {
+        "application/json": {
+          example: []
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
+  authMiddleware,
+  getStores,
+);
+
+router.get(
+  "/me",
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "내 가게를 조회합니다."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.responses[200] = {
+      description: "OK",
+      content: {
+        "application/json": {
+          examples: {
+            example: { $ref: "#/components/examples/storesExample" }
+          }
+        }
+      }
+    }
+    #swagger.responses[404] = {
+      description: "사용자 소유의 가게가 존재하지 않을 때",
+      content: {
+        "application/json": {
+          example: []
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
+  authMiddleware,
+  getMyStores,
+);
+
+router.get(
+  "/:storeId",
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "특정 가게를 조회합니다."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.parameters['storeId'] = {
+      description: '가게 아이디',
+      required: true,
+    }
+  */
+  /*
+    #swagger.responses[200] = {
+      description: "OK",
+      content: {
+        "application/json": {
+          examples: {
+            example: { $ref: "#/components/examples/storeExample" }
+          }
+        }
+      }
+    }
+    #swagger.responses[404] = {
+      description: "가게가 존재하지 않을 때",
+      content: {
+        "application/json": {
+          example: { message: "존재하지 않는 가게입니다." }
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
+  authMiddleware,
+  getStoreById,
+);
+
+router.get(
+  "/:storeId/members",
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "특정 가게의 직원 목록을 조회합니다."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.parameters['storeId'] = {
+      description: '가게 아이디',
+      required: true,
+    }
+  */
+  /*
+    #swagger.responses[403] = {
+      description: "가게 소유자가 아닐 때",
+      content: {
+        "application/json": {
+          example: { message: "가게 소유자만 조회할 수 있습니다." }
+        }
+      }
+    }
+    #swagger.responses[404] = {
+      description: "가게에 소속된 직원이 없을 때",
+      content: {
+        "application/json": {
+          example: []
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
+  authMiddleware,
+  getStoreMembers,
+);
+
+router.post(
+  "/",
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "가게를 생성합니다."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            $ref: "#/components/schemas/createStore"
+          }  
+        }
+      }
+    }
+  */
+  /*
+    #swagger.responses[201] = {
+      description: "Created",
+      content: {
+        "application/json": {
+          example: { message: "가게가 생성되었습니다." }
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
+  authMiddleware,
+  createStore,
+);
+
+router.post(
+  "/:storeId/members",
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "특정 가게에 직원을 추가합니다."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            $ref: "#/components/schemas/addMember"
+          }  
+        }
+      }
+    }
+  */
+  /*
+    #swagger.parameters['storeId'] = {
+      description: '가게 아이디',
+      required: true,
+    }
+  */
+  /*
+    #swagger.responses[200] = {
+      description: "OK",
+      content: {
+        "application/json": {
+          example: { message: "직원이 추가되었습니다." }
+        }
+      }
+    }
+    #swagger.responses[403] = {
+      description: "가게 소유자가 아닐 때",
+      content: {
+        "application/json": {
+          example: { message: "가게 소유자만 추가할 수 있습니다." }
+        }
+      }
+    }
+    #swagger.responses[404] = {
+      description: "존재하지 않는 직원일 때",
+      content: {
+        "application/json": {
+          example: { message: "존재하지 않는 직원입니다." }
+        }
+      }
+    }
+    #swagger.responses[409] = {
+      description: "이미 존재하는 직원일 때",
+      content: {
+        "application/json": {
+          example: { message: "이미 존재하는 직원입니다." }
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
+  authMiddleware,
+  addStoreMember,
+);
+
+router.delete(
+  "/:storeId/members/:memberId",
+  // #swagger.tags = ["Stores"]
+  // #swagger.description = "특정 가게의 직원을 삭제합니다."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.parameters['storeId'] = {
+      description: '가게 아이디',
+      required: true,
+    }
+      #swagger.parameters['memberId'] = {
+      description: '직원 아이디',
+      required: true,
+    }
+  */
+  /*
+    #swagger.responses[200] = {
+      description: "OK",
+      content: {
+        "application/json": {
+          example: { message: "직원이 삭제되었습니다." }
+        }
+      }
+    }
+    #swagger.responses[403] = {
+      description: "가게 소유자가 아닐 때",
+      content: {
+        "application/json": {
+          example: { message: "가게 소유자만 삭제할 수 있습니다." }
+        }
+      }
+    }
+      #swagger.responses[404] = {
+      description: "존재하지 않는 직원일 때",
+      content: {
+        "application/json": {
+          example: { message: "존재하지 않는 직원입니다." }
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+  */
+  authMiddleware,
+  deleteStoreMember,
+);
+
+export default router;
---
+++ src/services/stores.service.ts
@@ -0,0 +1,96 @@
+import { sql } from "kysely";
+import { nanoid } from "nanoid";
+
+import { db } from "@/db";
+import { CreateStorePayload } from "@/interfaces/stores.interface";
+
+export const getStoresService = async (userId?: string) => {
+  const stores = await db
+    .selectFrom("store")
+    .select(["id", "title", "location"])
+    .$if(!!userId, (q) => q.where("ownerId", "=", userId!))
+    .execute();
+
+  return stores;
+};
+
+export const getStoreByIdService = async (storeId: string) => {
+  const store = await db
+    .selectFrom("store")
+    .select(["title", "location", "contact"])
+    .where("id", "=", storeId)
+    .executeTakeFirst();
+
+  return store;
+};
+
+export const getStoreMembersService = async (storeId: string) => {
+  const members = await db
+    .selectFrom("storeMember")
+    .innerJoin("user", (join) =>
+      join.onRef("storeMember.userId", "=", "user.id").on("storeMember.storeId", "=", storeId),
+    )
+    .select(["user.id", "user.name", "user.contact"])
+    .execute();
+
+  return members;
+};
+
+export const createStoreService = async (payload: CreateStorePayload) => {
+  const { ownerId, title, location, contact, password } = payload;
+
+  const store = await db
+    .insertInto("store")
+    .values({ id: nanoid(8), ownerId, title, location, contact, password })
+    .executeTakeFirst();
+
+  return store;
+};
+
+export const addStoreMemberService = async (storeId: string, userId: string) => {
+  const result = await db.insertInto("storeMember").values({ storeId, userId }).executeTakeFirst();
+
+  return result;
+};
+
+export const deleteStoreMemberService = async (storeId: string, memberId: string) => {
+  const result = await db
+    .deleteFrom("storeMember")
+    .where("storeId", "=", storeId)
+    .where("userId", "=", memberId)
+    .executeTakeFirst();
+
+  return result;
+};
+
+export const isUserExistsService = async (userId: string) => {
+  const result = await db
+    .selectFrom("user")
+    .select(sql`1`.as("exists"))
+    .where("id", "=", userId)
+    .executeTakeFirst();
+
+  return result !== undefined;
+}
+
+export const isOwnerService = async (userId: string, storeId: string) => {
+  const result = await db
+    .selectFrom("store")
+    .select(sql`1`.as("exists"))
+    .where("id", "=", storeId)
+    .where("ownerId", "=", userId)
+    .executeTakeFirst();
+
+  return result !== undefined;
+};
+
+export const isStoreMemberService = async (userId: string, storeId: string) => {
+  const result = await db
+    .selectFrom("storeMember")
+    .select(sql`1`.as("exists"))
+    .where("userId", "=", userId)
+    .where("storeId", "=", storeId)
+    .executeTakeFirst();
+
+  return result !== undefined;
+};
