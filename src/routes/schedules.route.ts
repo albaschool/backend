137c451674e16131b6bd624f20dbd98dfe955f5e
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-12 22:25:46 +0900

    Feat: swagger 문서화

--- src/routes/schedules.route.ts
+++ src/routes/schedules.route.ts
@@ -10,24 +10,42 @@ router.use(authMiddleware);
 /** GET /schedules */
 router.get(
   "",
-  // #swagger.tags = ["Schedules"]
-  // #swagger.description = "내 일정을 조회합니다."
-  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+    #swagger.tags = ["Schedules"]
+    #swagger.description = "내 일정을 조회합니다."
+    #swagger.security = [{ bearerAuth: [] }]
+    #swagger.responses[404] = {
+      description: "일정이 존재하지 않을 때",
+      content: {
+        "application/json": {
+          example: []
+        }
+      }
+    }
+  */
   getSchedulesByUser,
 );
 
 /** /schedules/:storeId */
 router.get(
   "/:storeId",
-  // #swagger.tags = ["Schedules"]
-  // #swagger.description = "특정 가게의 일정을 조회합니다."
-  // #swagger.security = [{ bearerAuth: [] }]
   /*
+    #swagger.tags = ["Schedules"]
+    #swagger.description = "특정 가게의 일정을 조회합니다."
+    #swagger.security = [{ bearerAuth: [] }]
     #swagger.responses[403] = {
       description: "가게에 소속되어 있지 않을 때",
       content: {
         "application/json": {
           example: { message: "가게에 소속되어 있지 않습니다." }
+        }
+      }
+    }
+    #swagger.responses[404] = {
+      description: "일정이 존재하지 않을 때",
+      content: {
+        "application/json": {
+          example: []
         }
       }
     }
