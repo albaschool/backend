2e6bb1439a47679be24ee0bc5705e7557b97c328
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-14 22:57:56 +0900

    Fix: notification select때 title 추가

--- src/services/notifications.service.ts
+++ src/services/notifications.service.ts
@@ -3,7 +3,7 @@ import { db } from "@/db";
 export const getNotificationsByUserId = async (userId: string, limit: number) => {
   const notifications = await db
     .selectFrom("notification")
-    .select(["id", "content", "target", "isChecked", "createdAt"])
+    .select(["id", "content", "title", "target", "isChecked", "createdAt"])
     .where("userId", "=", userId)
     .limit(limit)
     .orderBy("createdAt", "desc")
