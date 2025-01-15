3eb99bb03d6bb254aeafad5ab0709fd106856138
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-15 10:25:59 +0900

    Feat: 알림 읽음 처리

--- src/controllers/notifications.controller.ts
+++ src/controllers/notifications.controller.ts
@@ -7,7 +7,7 @@ import { notificationsSessionManager as sessionManager } from "@/utils/session-m
 export const initializeSse = async (req: Request, res: Response) => {
   const session = await sessionManager.createSession(req.auth!.id, req, res, {
     userId: req.auth!.id,
-    storeId: "", // TODO: storeId를 어떻게 저장할지 결정
+    storeId: "", // TODO: storeId를 어떻게 저장할지 결정 저장 할 필요가 있나?
   });
 
   const notifications = await services.getNotificationsByUserId(req.auth!.id, 10);
@@ -16,6 +16,6 @@ export const initializeSse = async (req: Request, res: Response) => {
 };
 
 export const readNotifications = async (req: Request, res: Response) => {
-  // TODO: 읽음 처리를 한번에 처리할지, 알림 ID를 받아와서 하나씩 읽음 처리할지 결정
+  await services.readAllNotifications(req.auth!.id);
   res.status(200).json({ success: true });
 };
--- src/services/notifications.service.ts
+++ src/services/notifications.service.ts
@@ -11,3 +11,14 @@ export const getNotificationsByUserId = async (userId: string, limit: number) =>
 
   return notifications;
 };
+
+export const readAllNotifications = async (userId: string) => {
+  const result = await db
+    .updateTable("notification")
+    .set({ isChecked: true })
+    .where("userId", "=", userId)
+    .where("isChecked", "=", false)
+    .executeTakeFirst();
+
+  return result;
+}
