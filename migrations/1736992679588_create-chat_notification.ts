5306040935df270749c23b6116d127d13989a561
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-16 12:03:10 +0900

    Style: 워크플로우 테스트 겸 코드 포매팅 (#32)
    * Style: 코드 포매팅
    
    * Style: pnpm prettier:fix

--- migrations/1736992679588_create-chat_notification.ts
+++ migrations/1736992679588_create-chat_notification.ts
@@ -1,15 +1,14 @@
-import { type Kysely,sql } from 'kysely'
+import { type Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<unknown>): Promise<void> {
-	await db.schema
+  await db.schema
     .createTable("chat_notification")
     .addColumn("user_id", "varchar(12)", (col) => col.notNull())
-	.addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull())
-	.addForeignKeyConstraint("chat_notification_user_id", ["user_id"], "user", ['id'])
-	.execute();
+    .addColumn("created_at", "timestamp", (col) => col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull())
+    .addForeignKeyConstraint("chat_notification_user_id", ["user_id"], "user", ["id"])
+    .execute();
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-	await db.schema.dropTable("chat_notification").execute();
-
+  await db.schema.dropTable("chat_notification").execute();
 }
