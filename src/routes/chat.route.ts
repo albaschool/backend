d587cb1277f9636523ec8fd458cf81801e7f0775
AnDev77 <dnr0627@naver.com>
2025-01-20 18:58:49 +0900

    Fix : chat router 수정

--- src/routes/chat.route.ts
+++ src/routes/chat.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { chatRoomSave, getChatRoomDetail, setLastMessage } from "@/controllers/chat.contoller";
+import { chatRoomSave, getChatRoomDetail} from "@/controllers/chat.contoller";
 import authMiddleware from "@/middlewares/auth.middleware";
 const router = express.Router();
 
@@ -42,45 +42,6 @@ router.post(
     */
   authMiddleware,
   chatRoomSave,
-);
-
-router.put(
-  "/message/last",
-  // #swagger.tags = ["Chat"]
-  // #swagger.description = "마지막으로 읽은 메시지 설정."
-  // #swagger.security = [{ bearerAuth: [] }]
-  /*
-      #swagger.requestBody = {
-      required: true,
-      content: {
-          "application/json": {
-          schema: {
-              $ref: "#/components/schemas/saveLastMessage"
-          }  
-          }
-      }
-      }
-  */
-  /*
-      #swagger.responses[201] = {
-      description: "Created",
-      content: {
-          "application/json": {
-          example: { message: "마지막 메시지가 변경 됐습니다." }
-          }
-      }
-      }
-      #swagger.responses[500] = {
-      description: "Internal Server Error",
-      content: {
-          "application/json": {
-          example: { message: "Internal server error" }
-          }
-      }
-      }
-  */
-  authMiddleware,
-  setLastMessage,
 );
 
 //채팅방 상세 조회
