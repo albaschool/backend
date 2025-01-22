f1551987015faa079f40c15d745c52222dcbcfc5
AnDev77 <dnr0627@naver.com>
2025-01-22 19:21:01 +0900

    Fix : 경로 오류 해결 및 스타일 적용

---
+++ src/routes/chat.route.ts
@@ -0,0 +1,124 @@
+import express from "express";
+
+import { chatRoomSave, getChatRoomDetail, setLastMessage } from "@/controllers/chat.controller";
+import authMiddleware from "@/middlewares/auth.middleware";
+const router = express.Router();
+
+// 채팅방 생성
+router.post(
+  "/",
+  // #swagger.tags = ["Chat"]
+  // #swagger.description = "채팅방 생성."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+        #swagger.requestBody = {
+        required: true,
+        content: {
+            "application/json": {
+            schema: {
+                $ref: "#/components/schemas/createChatRoom"
+            }  
+            }
+        }
+        }
+    */
+  /*
+        #swagger.responses[201] = {
+        description: "Created",
+        content: {
+            "application/json": {
+            example: { message: "채팅방 생성이 완료 됐습니다." }
+            }
+        }
+        }
+        #swagger.responses[500] = {
+        description: "Internal Server Error",
+        content: {
+            "application/json": {
+            example: { message: "Internal server error" }
+            }
+        }
+        }
+    */
+  authMiddleware,
+  chatRoomSave,
+);
+
+router.put(
+  "/message/last",
+  // #swagger.tags = ["Chat"]
+  // #swagger.description = "마지막으로 읽은 메시지 설정."
+  // #swagger.security = [{ bearerAuth: [] }]
+  /*
+      #swagger.requestBody = {
+      required: true,
+      content: {
+          "application/json": {
+          schema: {
+              $ref: "#/components/schemas/saveLastMessage"
+          }  
+          }
+      }
+      }
+  */
+  /*
+      #swagger.responses[201] = {
+      description: "Created",
+      content: {
+          "application/json": {
+          example: { message: "마지막 메시지가 변경 됐습니다." }
+          }
+      }
+      }
+      #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+          "application/json": {
+          example: { message: "Internal server error" }
+          }
+      }
+      }
+  */
+  authMiddleware,
+  setLastMessage,
+);
+
+//채팅방 상세 조회
+router.get(
+  "/:id",
+  /*
+    #swagger.tags = ["Chat"]
+    #swagger.description = "채팅방의 상세 내역을 가져옵니다."
+    #swagger.security = [{ bearerAuth: [] }]
+    #swagger.responses[200] = {
+      description: "OK",
+      content: {
+        "application/json": {
+          examples: {
+            example: { $ref: "#/components/examples/chatRoomDetailExample" }
+          }
+        }
+      }
+    }
+    #swagger.responses[404] = {
+      description: "가게가 존재하지 않을 때",
+      content: {
+        "application/json": {
+          example: []
+        }
+      }
+    }
+    #swagger.responses[500] = {
+      description: "Internal Server Error",
+      content: {
+        "application/json": {
+          example: { message: "Internal server error" }
+        }
+      }
+    }
+    */
+  authMiddleware,
+  getChatRoomDetail,
+);
+
+export default router;
