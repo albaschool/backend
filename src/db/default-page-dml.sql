7f5554c5e3941366cc53011e861af32d3924c09e
AnDev77 <dnr0627@naver.com>
2025-01-24 16:58:15 +0900

    Feat : 기본 교육 페이지 dml 파일 생성

---
+++ src/db/default-page-dml.sql
@@ -0,0 +1,34 @@
+insert into default_page (type, title) values("교육/학원", "수업 보조 및 학생 관리 방법");
+insert into default_page (type, title) values("교육/학원", "교육 자료 준비 및 관리 요령");
+insert into default_page (type, title) values("교육/학원",  "출석 체크 및 학사 관리 시스템 사용법");
+insert into default_page (type, title) values("교육/학원", "교육 기자재 사용법");
+insert into default_page (type, title) values("교육/학원", "긴급 상황 (안전사고 등) 대처 방법");
+insert into default_page (type, title) values("교육/학원", "개인정보 보호 및 보안 규정");
+
+insert into default_page (type, title) values("판매/서비스", "프로모션 및 할인 정책");
+insert into default_page (type, title) values("판매/서비스", "상품 지식 및 설명 방법");
+insert into default_page (type, title) values("판매/서비스", "고객 응대 및 상담 기술");
+insert into default_page (type, title) values("판매/서비스", "결제 시스템 사용법");
+insert into default_page (type, title) values("판매/서비스", "매장 청소 및 정리방법");
+
+insert into default_page (type, title) values("음식점/카페", "메뉴 및 레시피");
+insert into default_page (type, title) values("음식점/카페", "주문 접수 및 POS 시스템 사용법");
+insert into default_page (type, title) values("음식점/카페", "주방 기구 사용법 및 안전 수칙");
+insert into default_page (type, title) values("음식점/카페", "위생 관리 지침");
+insert into default_page (type, title) values("음식점/카페", "고객 응대 매뉴얼");
+insert into default_page (type, title) values("음식점/카페", "매장 청소 및 정리방법");
+
+insert into default_page (type, title) values("편의점/마트", "POS 시스템 사용법");
+insert into default_page (type, title) values("편의점/마트", "상품 진열 및 재고 관리 방법");
+insert into default_page (type, title) values("편의점/마트", "연령 제한 상품 판매 규정");
+insert into default_page (type, title) values("편의점/마트", "택배 수납 절차");
+insert into default_page (type, title) values("편의점/마트", "폐기 상품 처리 방법");
+insert into default_page (type, title) values("편의점/마트", "매장 청소 및 정리방법");
+
+insert into default_page (type, title) values("여가/엔터테인먼트", "시설 이용 안내 및 규칙");
+insert into default_page (type, title) values("여가/엔터테인먼트", "티켓 발권 및 예약 시스템 사용법");
+insert into default_page (type, title) values("여가/엔터테인먼트", "안전 관리 및 긴급 상황 대처 방법");
+insert into default_page (type, title) values("여가/엔터테인먼트", "장비 사용법 (예: 영사기, 게임기 등)");
+insert into default_page (type, title) values("여가/엔터테인먼트", "고객 안내 및 서비스 제공 요령");
+insert into default_page (type, title) values("여가/엔터테인먼트", "청소 및 위생 관리 지침");
+insert into default_page (type, title) values("여가/엔터테인먼트", "이벤트 및 프로모션 안내");
