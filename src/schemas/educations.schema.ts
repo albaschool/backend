ca707cea13feecd784bbc2c423b78da74ef8e011
Lee Taehoon <gb4tt3ry@gmail.com>
2025-01-30 04:45:11 +0900

    Feat: 강의 자료 생성, 수정 스키마 생성

--- src/schemas/educations.schema.ts
+++ src/schemas/educations.schema.ts
@@ -5,3 +5,21 @@ export const eduIdParamsSchema = z.object({
     eduId: z.string().length(12),
   }),
 });
+
+export const createEducationSchema = z.object({
+  body: z.object({
+    title: z.string(),
+    content: z.string(),
+  }),
+  file: z.object({
+    img: z
+      .custom<Express.Multer.File>()
+      .refine((file) => file.size <= 1024 * 1024 * 5, "파일 크기는 5MB 이하여야 합니다.")
+      .refine(
+        (file) => ["image/jpeg", "image/png", "image/webp"].includes(file.mimetype),
+        "지원되는 이미지 형식은 jpg, png, webp입니다.",
+      ),
+  }),
+});
+
+export const updateEducationSchema = createEducationSchema.partial();
