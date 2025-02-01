cc4b2a9d59e216fa95d774f1697a292b0f05c6c6
Lee Taehoon <gb4tt3ry@gmail.com>
2025-02-01 23:14:41 +0900

    Feat: json limit 50mb로 상향

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,11 @@ const app = express();
 const server = http.createServer(app);
 socketRun(server);
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "50mb",
+  }),
+);
 app.use(httpLogger);
 
 app.use(
